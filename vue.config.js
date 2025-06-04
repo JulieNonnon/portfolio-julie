fae0008e4e22cd618e2cb049af82a384b83144d3
JulieNonnon <julie.nonnon@gmail.com>
2025-06-04 10:18:24 +0200

    added publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require("path");
 
 module.exports = defineConfig({
+  publicPath: '/portfolio-julie/', // ne pas oublier la config de la base pour le déploiement Github Pages
   transpileDependencies: true,
     configureWebpack: {
       resolve: {
